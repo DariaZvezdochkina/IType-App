35da855cea142f44ed45114a60e51657a1fe0372
DariaZvezdochkina <d.zvezdochkina@mail.ru>
2022-02-21 14:36:13 +0200

    Initial Commit

---
+++ IType App/IType_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IType_AppApp.swift
+//  IType App
+//
+//  Created by Daria Zvezdochkina on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct IType_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
