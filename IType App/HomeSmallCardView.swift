1a5cfb86c12bbfbf4fed4a899351e44c7c9432f1
DariaZvezdochkina <d.zvezdochkina@mail.ru>
2022-03-25 14:49:45 +0200

    Delete Firebase Package

--- IType App/HomeSmallCardView.swift
+++ IType App/HomeSmallCardView.swift
@@ -13,20 +13,20 @@ struct HomeSmallCardView: View {
     RoundedRectangle(cornerRadius: 30)
       .shadow(color: Color("MainFrameColor"), radius: 4)
       .overlay(
-    VStack(alignment: .leading) {
-      
-      Image("\(card.image)")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-        .frame(width: 60, height: 60)
-        .clipped()
-      Text(card.title)
-        .font(.headline)
-      
-    }
-    .foregroundColor(.black)
-    .padding()
-    )
+        VStack(alignment: .leading) {
+          
+          Image("\(card.image)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 60, height: 60)
+            .clipped()
+          Text(card.title)
+            .font(.headline)
+          
+        }
+          .foregroundColor(.black)
+          .padding()
+      )
       .foregroundColor(Color.white)
       .frame(width: 150, height: 150)
   }
